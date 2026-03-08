998e6c80a100f4040ee6056111d1d86a45f05660
uiaaaa <s6804062612013@email.kmutnb.ac.th>
2026-03-08 11:19:58 +0700

    lab10no3_1.c

---
+++ lab10no3_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Books{ 
+    int BookID; 
+    char BookTitle[50]; 
+} ComputerBook; 
+void DisplayData(struct Books CBook); 
+
+int main() { 
+    printf("Enter Book ID : ");      
+    scanf("%d", &ComputerBook.BookID);  
+
+    printf("Enter Book Title : ");   
+    scanf("%s", ComputerBook.BookTitle); 
+
+    DisplayData(ComputerBook); 
+    return 0; 
+} 
+
+void DisplayData(struct Books CBook){ 
+    printf("Book ID : %d \n", CBook.BookID); 
+    printf("Book Title : %s \n", CBook.BookTitle); 
+}
+//void DisplayData(struct Books CBook) คือฟังก์ชั่นแสดงผล BookID กับ BookTitle
+//พารามิเตอร์ที่รับเข้ามาคือ struct Books จาก ComputerBook
